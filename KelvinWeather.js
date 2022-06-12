fda4cf2c868afa9ba1d2b54d5e68e7e9f4810873
Dennis Rade <100362836+Sizzlon@users.noreply.github.com>
2022-06-12 22:23:49 +0200

    Create KelvinWeather.js

---
+++ KelvinWeather.js
@@ -0,0 +1,15 @@
+
+//this is Kelvin
+const kelvin = 293;
+
+
+// this is celsius
+var celsius = kelvin - 273;
+
+// this is fahrenheit
+var fahrenheit = celsius *(9/5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+
+console.log('The temperature is '+fahrenheit+' degrees Fahrenheit.');
+
